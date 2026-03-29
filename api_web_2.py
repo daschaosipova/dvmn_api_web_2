589b33dce788d1ff756f0c27257ea182b718f8c1
daschaosipova <daschaosipova@yandex.com>
2026-03-29 08:00:27 +0300

    Create api_web_2.py

---
+++ api_web_2.py
@@ -0,0 +1,76 @@
+import requests
+import os
+import sys
+from dotenv import load_dotenv
+from urllib.parse import urlparse
+
+
+def shorten_link(token, url, long_link):
+    payload = {"url": long_link}
+    headers = {"Authorization": token, "Content-Type": "application/json"}
+    response = requests.post(url, json=payload, headers=headers)
+    response.raise_for_status()
+    parsed = urlparse(response.json()["shorturl"])
+    short_link = parsed[1] + parsed[2]
+    return short_link
+
+
+def count_clicks(token, url, short_link):
+    payload = {"short": short_link}
+    headers = {"Authorization": token, "Content-Type": "application/json"}
+    response = requests.get(url, headers=headers, params=payload)
+    response.raise_for_status()
+    clicks_count = response.json()["data"]["clicks"]
+    return clicks_count
+
+
+def is_bitlink(token, url, link):
+    payload = {"short": link}
+    headers = {"Authorization": token, "Content-Type": "application/json"}
+    response = requests.get(url, headers=headers, params=payload)
+    response.raise_for_status()
+    if not response.json()["error"]:
+        return True
+
+
+def main():
+    load_dotenv(".env")
+    user_link = input("Введите ссылку: ")
+    try:
+        response = requests.get(user_link)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError:
+        print("Произошла ошибка, введена неверная ссылка")
+        sys.exit("Программа остановлена из-за ошибки.")
+    except requests.exceptions.MissingSchema:
+        print("Произошла ошибка, введена неверная ссылка")
+        sys.exit("Программа остановлена из-за ошибки.")
+    link_is_short = is_bitlink(
+        token=os.getenv("TOKEN"),
+        url="https://clc.li/api/urls?limit=2&page=1&order=date",
+        link=user_link,
+    )
+    if link_is_short:
+        clicks_count = count_clicks(
+            token=os.getenv("TOKEN"),
+            url="https://clc.li/api/urls?limit=2&page=1&order=date",
+            short_link=user_link,
+        )
+        print("Количество кликов по ссылке:", clicks_count)
+    else:
+        short_link = shorten_link(
+            token=os.getenv("TOKEN"),
+            url="https://clc.li/api/url/add",
+            long_link=user_link,
+        )
+        print("Короткая ссылка", short_link)
+        clicks_count = count_clicks(
+            token=os.getenv("TOKEN"),
+            url="https://clc.li/api/urls?limit=2&page=1&order=date",
+            short_link=short_link,
+        )
+        print("Количество кликов по ссылке:", clicks_count)
+
+
+if __name__ == "__main__":
+    main()
